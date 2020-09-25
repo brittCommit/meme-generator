a9a39b660463ae58876bff59f569a13d1dd4ed16
Brittney Huttner <brittneyhuttner@gmail.com>
2020-09-25 16:55:49 -0700

    added bones to render text

---
+++ src/MemeGenerator.js
@@ -0,0 +1,16 @@
+import React, {Component} from "react"
+
+class MemeGenerator extends Component {
+    constructor() {
+        super()
+        this.state ={}
+    }
+
+    render() {
+        return (
+            <h1>There will be a meme here!</h1>
+        )
+    }
+}
+
+export default MemeGenerator
